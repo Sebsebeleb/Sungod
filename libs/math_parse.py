ae88ec0cae6da057e1ab8b8e15419d1c5fd6a70b
Sebsebeleb <seb@terminator.net>
2013-10-10 22:33:56 +0200

    added more allowed operations to eval parser

--- libs/math_parse.py
+++ libs/math_parse.py
@@ -1,12 +1,20 @@
 import ast
 
-allowed = [ast.Expression, ast.Num, ast.BinOp, ast.Add, ast.Sub,
-           ast.Mult, ast.Div, ast.Pow, ast.BitXor, ast.Add]
+allowed = (ast.Num, ast.Str, ast.List, ast.Tuple, ast.Set, ast.Dict,
+           ast.Ellipsis, ast.Expression,
+           ast.UnaryOp, ast.BinOp, ast.BoolOp, ast.Compare,
+           ast.Eq, ast.NotEq, ast.Lt, ast.LtE, ast.Gt, ast.GtE, ast.Is,
+           ast.IsNot, ast.In, ast.NotIn,
+           ast.UAdd, ast.USub, ast.Not, ast.Invert, ast.Add, ast.Sub,
+           ast.Mult, ast.Div, ast.FloorDiv, ast.Mod, ast.Pow, ast.LShift,
+           ast.RShift, ast.BitOr, ast.BitXor, ast.BitAnd, ast.And, ast.Or
+           )
 
 
 def parse(string):
     tree = ast.parse(string, mode='eval')
-    nodes = ast.walk(tree)  # Shouldn't this be "climb(tree)" instead?
+    nodes = list(ast.walk(tree))  # Shouldn't this be "climb(tree)" instead?
+    print nodes
     if not all((type(i) in allowed for i in nodes)):
         # Evil string, do not allow
         s = "No fishy business allowed here!"
